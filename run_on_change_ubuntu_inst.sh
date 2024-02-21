86e5025e7d86843ce84cb83ab64b37308131de82
efficacy38 <efficacy38@gmail.com>
2024-02-22 03:56:15 +0800

    feat: add my installed binary file

--- run_on_change_ubuntu_inst.sh
+++ run_on_change_ubuntu_inst.sh
@@ -13,14 +13,12 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | \
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | \
 	sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
-# setup glab cli
-curl -L https://packages.gitlab.com/glab/glab/gpgkey | sudo apt-key add -
-sudo curl -o /etc/apt/sources.list.d/glab.list https://packages.gitlab.com/glab/glab/ubuntu/$(lsb_release -cs)/glab.list
-
-
 # install packages
 sudo apt-get update
-sudo apt-get install -y glab gh ripgrep bat libnotify-bin
+sudo apt-get install -y glab gh ripgrep bat libnotify-bin snapd
+
+# install glab
+sudo snap install glab --classic
 
 # install rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
